612fc9425fb08180d37c22f5a68254b01d0797db
Hai Zhu <35182391+cocolato@users.noreply.github.com>
2023-06-14 22:01:56 -0600

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -100,8 +100,11 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    git
+    gcc
     vim
     thefuck
+    vscode
   ];
 
   programs.zsh = {
